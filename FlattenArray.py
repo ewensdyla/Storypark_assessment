0e29d1b468265c7feb4484a5c7b303c33f5a14fe
Dylan Ewens <ewensdyla@ecs.vuw.ac.nz>
2019-09-20 17:03:37 +1200

    Tests all pass
    My own recursive method implemented along with tests.

--- FlattenArray.py
+++ FlattenArray.py
@@ -1,6 +1,3 @@
-import numpy
-
-
 def flatten(init_array):
     """
     Takes an array which may contain nested arrays and returns a
@@ -8,6 +5,16 @@ def flatten(init_array):
     :param init_array: the array to be flattened.
     :return: the flattened one dimensional array.
     """
-    temp_array = numpy.array(init_array)
-    return list(temp_array.flatten())
+    # initialise the array to be returned
+    flat_array = []
+
+    # determine whether the value is an int or a list then either append or recurse
+    for elem in init_array:
+        if isinstance(elem, int):
+            flat_array.append(elem)
+
+        elif isinstance(elem, list):
+            flat_array += flatten(elem)
+
+    return flat_array
 
