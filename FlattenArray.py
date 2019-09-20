6afd1210c9e3ec40824e4c4b9ab4e5c3dbf343d1
Dylan Ewens <ewensdyla@ecs.vuw.ac.nz>
2019-09-20 16:22:09 +1200

    intertools library found and implemented
    Method works correctly, but test suite will be written

--- FlattenArray.py
+++ FlattenArray.py
@@ -1,1 +1,12 @@
+import itertools
+
+
 def flatten(init_array):
+    """
+    Takes an array which may contain nested arrays and returns a
+    flattened one dimensional array.
+    :param init_array: the array to be flattened.
+    :return: the flattened one dimensional array.
+    """
+    return list(itertools.chain.from_iterable(init_array))
+
