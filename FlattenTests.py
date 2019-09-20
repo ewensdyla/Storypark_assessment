00bb5e63ff25782d7f4904780dd2fa978f9fdc30
Dylan Ewens <ewensdyla@ecs.vuw.ac.nz>
2019-09-20 16:39:46 +1200

    Tests show method does not work when first dimension array is just an int

---
+++ FlattenTests.py
@@ -0,0 +1,38 @@
+import unittest
+import FlattenArray
+
+
+class Tests(unittest.TestCase):
+    def test_nested_array_01(self):
+        array = [[1], [2], 3]
+        flat_array = FlattenArray.flatten(array)
+        expected = [1, 2, 3]
+        self.assertTrue(flat_array == expected)
+
+    def test_nested_array_02(self):
+        array = [[1], [2], [3]]
+        flat_array = FlattenArray.flatten(array)
+        expected = [1, 2, 3]
+        self.assertTrue(flat_array == expected)
+
+    def test_nested_array_03(self):
+        array = [1, [2, 3], [4]]
+        flat_array = FlattenArray.flatten(array)
+        expected = [1, 2, 3, 4]
+        self.assertTrue(flat_array == expected)
+
+    def test_nested_array_04(self):
+        array = [[1], [2, 3], [4]]
+        flat_array = FlattenArray.flatten(array)
+        expected = [1, 2, 3, 4]
+        self.assertTrue(flat_array == expected)
+
+    def test_nested_array_05(self):
+        array = [[], [1], [2, 3], [4]]
+        flat_array = FlattenArray.flatten(array)
+        expected = [1, 2, 3, 4]
+        self.assertTrue(flat_array == expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
